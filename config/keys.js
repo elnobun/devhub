5d0e4e9f97c739a401a4a48ee38a28174e8d4681
Ellis Enobun <elnobun@gmail.com>
2018-04-29 17:31:16 +0100

    added "secret key" for jwt signature token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 /* Import mongodb for local connection */
 module.exports = {
-  mongoURI: "mongodb://elnobun:enobun@ds151169.mlab.com:51169/devhub"
+  mongoURI: "mongodb://elnobun:enobun@ds151169.mlab.com:51169/devhub",
+  secretOrKey: "YOUR_SECRET_KEY"
 };
