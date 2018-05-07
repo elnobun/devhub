1797968876be136eff8859799df2d89ca6083a46
Ellis Enobun <elnobun@gmail.com>
2018-05-07 22:27:02 +0100

    added validation for user registration, login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 /* Import registration validator */
 const validateRegistration = require("../../validation/register");
+/* Import Login validator */
+const validateLogin = require("../../validation/login");
 /* Import files */
 const secretKey = require("../../config/keys").secretOrKey;
 const User = require("../../models/User");
@@ -36,7 +38,7 @@ router.post("/register", (req, res) => {
   // Check if user email already exist in the database
   User.findOne({ email: req.body.email }).then(email => {
     if (email) {
-      return res.status(400).json((errors.email = "Email already exist"));
+      return res.status(400).json({EmailError: "email already exist"});
     } else {
       // Create an avatar variable, that details the size, rating and default
       // of the user avatar. It uses gravatar to extract the users profile pic
@@ -75,13 +77,20 @@ router.post("/register", (req, res) => {
  * @access:  PUBLIC
  */
 router.post("/login", (req, res) => {
+  // Validate user login
+  const { errors, isValid } = validateLogin(req.body);
+  // Check validation for name & email
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Check if the user already exist through their email
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ msg: "user not found" });
+      return res.status(404).json({ LoginError: "email or password is incorrect" });
     }
     // Check if passowrd is the same as the one in database.
     bcrypt.compare(password, user.password).then(isMatch => {
@@ -97,7 +106,7 @@ router.post("/login", (req, res) => {
           });
         });
       } else {
-        return res.status(400).json({ msg: "email or passowrd is incorrect" });
+        return res.status(400).json({ LoginError: "email or password is incorrect" });
       }
     });
   });
