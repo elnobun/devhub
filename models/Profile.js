1061871e148f5189ef6281d02adf55c16edfdc70
Ellis Enobun <elnobun@gmail.com>
2018-05-08 16:18:04 +0100

    user profile model

---
+++ models/Profile.js
@@ -0,0 +1,125 @@
+/* Import mongoose and Schema */
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/* Create Profile Schema */
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    refs: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 20
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  githubrepo: {
+    type: String
+  },
+  // Developer Experience
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  // Educational background
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldOfStudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  // Social NetWork
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+});
+
+const Profile = mongoose.model("profile", ProfileSchema);
+mongoose.exports = Profile;
