2510abe5fc27bd86bc5ca4fc5db10eb2e71d4c77
Ellis Enobun <elnobun@gmail.com>
2018-06-22 16:31:55 +0100

    updated app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React, { Component, Fragment } from 'react';
-import { Route } from 'react-router-dom';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import React, { Component, Fragment } from "react";
+import { Route } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
+        <Navbar />
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Footer />
       </Fragment>
     );
   }
